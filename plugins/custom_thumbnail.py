f0e4771c254eb3ba8783919cd6ea006bc0401ed8
vansh876 <137757076+vansh876@users.noreply.github.com>
2023-08-20 12:57:59 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -35,7 +35,7 @@ from plugins.database.add import add_user_to_database
 from plugins.settings.settings import *
 
 
-@Client.on_message(filters.private & filters.photo & ~filters.edited)
+@Client.on_message(filters.private & filters.photo)
 async def photo_handler(bot: Client, event: Message):
     if not event.from_user:
         return await event.reply_text("I don't know about you sar :(")
